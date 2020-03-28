3fe131072cdd7f643da49dcdf83f96dbe21f11c0
mhilmi999 <54462717+mhilmi999@users.noreply.github.com>
2020-03-28 20:36:14 +0700

    Add files via upload

---
+++ application/models/Account.php
@@ -0,0 +1,80 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Model {
+
+	function insertNewUser(){
+		$q = "INSERT INTO users (nama,username,password,email,date_updated)VALUES (
+			'". $this->input->post('nama') ."',
+			'". $this->input->post('username') ."',
+			'". $this->input->post('password') ."',
+			'". $this->input->post('email') ."',
+			now()
+		)";
+		$this->db->query($q);
+		$id_user	=	$this->db->insert_id();
+		$peran	=	$this->input->post('peran');
+		if ($peran == '1') {
+
+			$q2 = "INSERT INTO editor (id_user,date_updated)VALUES (
+			'". $id_user ."', now()
+			)";
+			$this->db->query($q2);
+
+			$q3 = "INSERT INTO member (id_user,id_grup,date_updated)VALUES (
+			'". $id_user ."','". $peran ."', now()
+			)";
+			$this->db->query($q3);
+
+		} else {
+
+			$q2 = "INSERT INTO reviewer (id_user,date_updated)VALUES (
+			". $id_user .", now()
+			)";
+			$this->db->query($q2);
+
+			$q3 = "INSERT INTO member (id_user,id_grup,date_updated)VALUES (
+			'". $id_user ."','". $peran ."', now()
+			)";
+			$this->db->query($q3);
+
+		}
+		return $id_user;
+	}
+	function getIdUser(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$q = "SELECT * FROM users WHERE username='$username' and password='$password'";
+		//$q = "SELECT * FROM users WHERE username='". $this->input->post('username') ."',"."password='". $this->input->post('password') ."'";
+		$res = $this->db->query($q);
+		$users = $res->result_array();
+
+		if (count($users)>0) {
+			return $users[0]['id_user'];
+		}
+		return -1;
+	}
+	/*
+	function getPeranUser(){
+		$q = "SELECT * FROM member WHERE id_grup and id_user";
+		//$q = "SELECT * FROM users WHERE username='". $this->input->post('username') ."',"."password='". $this->input->post('password') ."'";
+		$res = $this->db->query($q);
+		$member = $res->result_array();
+
+		if (count($member)>0) {
+			return $member[0]['id_grup'];
+		}
+		return -1;
+	}
+	*/
+	function getPeranUser($id_user = -1 ){
+		
+		$q = "SELECT * FROM member WHERE id_user ='$id_user'";
+		$res = $this->db->query($q);
+		//$peran	=	$this->input->post('peran');
+		$peran = $res->result_array();
+		return $peran[0]['id_grup'];
+	}
+	
+}
+?>
---
+++ application/views/reviewer/AcceptTask.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accept Task</title>
+</head>
+<body>
+	<h1>From Penerimaan Task</h1>
+	<form>
+		<!-- Judul : <input type="text" name="input">
+		Kata Kunci : <input type="text" name="katkun"> -->
+		<a href='http://localhost/ereview/index.php/manageassignedtask/submitreview'>Submit Review</a><br>
+	</form>
+</body>
+</html>
