d6cc5b8bad51116b56e23aad1e6fe59c588bd756
mhilmi999 <54462717+mhilmi999@users.noreply.github.com>
2020-03-30 16:42:48 +0700

    Update EditorCtl.php

--- application/controllers/EditorCtl.php
+++ application/controllers/EditorCtl.php
@@ -1,25 +1,16 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class EditorCtl extends CI_Controller {
+class EditorCtl extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['username'] = $this->db->get_where('users', ['email' =>
+		$this->session->userdata('email')])->row_array();
+		echo 'Anda telah masuk sebagai Editor' . $data['username'];
+		echo '         ';
+		echo anchor('AccountCtl/logout', 'logout');
+		$this->load->view('editor/BerandaEditor');
 	}
 }
